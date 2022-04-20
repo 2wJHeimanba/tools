be9c946a5ed63401cae26aaf211504942fae937f
2wj宁静致远 <zwj18506698903@163.com>
2022-04-20 22:57:08 +0800

    scroll开发

--- components/scrollbar/dist/scrollbar.esm.js
+++ components/scrollbar/dist/scrollbar.esm.js
@@ -3,25 +3,55 @@ function Scroll(el) {
         el = document.querySelector(el);
     }
     var scrollDom;
+    var scrollParentDom;
     var scrollBoxHeight = el.clientHeight; //可见区域
     var contentBoxHeight = el.scrollHeight; //内容的全部高度
     if (scrollBoxHeight < contentBoxHeight) { // 判断是否有滚动条
-        var parentDom = document.createElement("div");
-        parentDom.setAttribute("class", "vans-scroll-parent");
-        parentDom.appendChild(el);
+        scrollParentDom = document.createElement("div");
+        scrollParentDom.setAttribute("class", "vans-scroll-parent");
+        scrollParentDom.appendChild(el);
         var hideDom = document.createElement("div");
         hideDom.setAttribute("class", "vans-hide-dom");
         scrollDom = document.createElement("div");
         scrollDom.setAttribute("class", "vans-scroll-thumb");
         hideDom.appendChild(scrollDom);
-        parentDom.appendChild(hideDom);
-        document.body.appendChild(parentDom);
+        scrollParentDom.appendChild(hideDom);
+        document.body.appendChild(scrollParentDom);
     }
     //滚动条高度
     scrollDom.style.height = ((scrollBoxHeight * scrollBoxHeight) / contentBoxHeight) + "px";
     el.addEventListener("scroll", function () {
         var top = ((el.scrollTop * scrollBoxHeight) / contentBoxHeight) + "px";
         scrollDom.style.top = top;
+    });
+    scrollParentDom.addEventListener("mouseenter", function () {
+        setTimeout(function () {
+            var temporaryOpacity = 0;
+            var timein = setInterval(function () {
+                temporaryOpacity += 0.015;
+                if (temporaryOpacity >= 1) {
+                    clearInterval(timein);
+                    temporaryOpacity = 1;
+                }
+                scrollDom.style.setProperty("opacity", "".concat(temporaryOpacity.toFixed(2)));
+            }, 4);
+        }, 300);
+    });
+    scrollParentDom.addEventListener("mouseleave", function () {
+        setTimeout(function () {
+            var temporaryOpacity = 1;
+            var timeout = setInterval(function () {
+                temporaryOpacity -= 0.015;
+                if (temporaryOpacity <= 0) {
+                    clearInterval(timeout);
+                    temporaryOpacity = 0;
+                }
+                scrollDom.style.setProperty("opacity", "".concat(temporaryOpacity.toFixed(2)));
+            }, 4);
+        }, 300);
+    });
+    scrollDom.addEventListener("mousedown", function () {
+        console.log("wenjfkdls");
     });
 }
 window.scroll = Scroll;
--- components/scrollbar/src/index.ts
+++ components/scrollbar/src/index.ts
@@ -5,20 +5,21 @@ function Scroll(el:string|Element):void{
         el = document.querySelector(el)
     }
     let scrollDom:Element|HTMLElement;
+    let scrollParentDom:Element|HTMLElement;
 
     let scrollBoxHeight = el.clientHeight;  //可见区域
     let contentBoxHeight = el.scrollHeight; //内容的全部高度
     if(scrollBoxHeight < contentBoxHeight){ // 判断是否有滚动条
-        let parentDom = document.createElement("div");
-        parentDom.setAttribute("class","vans-scroll-parent");
-        parentDom.appendChild(el);
+        scrollParentDom = document.createElement("div");
+        scrollParentDom.setAttribute("class","vans-scroll-parent");
+        scrollParentDom.appendChild(el);
         let hideDom = document.createElement("div");
         hideDom.setAttribute("class","vans-hide-dom");
         scrollDom = document.createElement("div");
         scrollDom.setAttribute("class","vans-scroll-thumb");
         hideDom.appendChild(scrollDom);
-        parentDom.appendChild(hideDom);
-        document.body.appendChild(parentDom);
+        scrollParentDom.appendChild(hideDom);
+        document.body.appendChild(scrollParentDom);
     }
     //滚动条高度
     (scrollDom as HTMLElement).style.height = ((scrollBoxHeight * scrollBoxHeight) / contentBoxHeight) + "px";
@@ -26,6 +27,38 @@ function Scroll(el:string|Element):void{
         let top = (((el as HTMLElement).scrollTop * scrollBoxHeight) / contentBoxHeight) + "px";
         (scrollDom as HTMLElement).style.top = top
     });
+
+    scrollParentDom.addEventListener("mouseenter",function(){//进入显示滚动条
+        setTimeout(()=>{
+            let temporaryOpacity:number = 0;
+            let timein = setInterval(()=>{
+                temporaryOpacity += 0.015;
+                if(temporaryOpacity >= 1){
+                    clearInterval(timein);
+                    temporaryOpacity = 1;
+                }
+                (scrollDom as HTMLElement).style.setProperty("opacity",`${temporaryOpacity.toFixed(2)}`);
+            },4);
+        },300)
+    });
+
+    scrollParentDom.addEventListener("mouseleave",function(){//离开隐藏滚动条
+        setTimeout(()=>{
+            let temporaryOpacity:number = 1;
+            let timeout = setInterval(()=>{
+                temporaryOpacity -= 0.015;
+                if(temporaryOpacity <= 0){
+                    clearInterval(timeout);
+                    temporaryOpacity = 0;
+                }
+                (scrollDom as HTMLElement).style.setProperty("opacity",`${temporaryOpacity.toFixed(2)}`);
+            },4);
+        },300);        
+    });
+
+    scrollDom.addEventListener("mousedown",function(){
+        console.log("wenjfkdls")
+    })
 
 }
 
